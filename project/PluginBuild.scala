e5b002ac2d93ddde794259bd2c1d8b3d5623eb01
Charles Kubicek <charles.kubicek@digital.hmrc.gov.uk>
2015-04-13 15:07:27 +0100

    [NIJ] releaseing 1.5.0

--- project/PluginBuild.scala
+++ project/PluginBuild.scala
@@ -26,7 +26,7 @@ import scala.util.matching.Regex
 object PluginBuild extends Build {
 
   val pluginName = "sbt-auto-build"
-  val pluginVersion = "0.1.5-SNAPSHOT"
+  val pluginVersion = "0.1.5"
 
   private val standardSettings: Seq[Setting[_]] =
     scalaSettings ++
