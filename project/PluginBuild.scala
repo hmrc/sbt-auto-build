8a5d6c264586104037e648a35de02e2409489c77
Charles Kubicek <charles.kubicek@digital.hmrc.gov.uk>
2015-04-13 15:06:56 +0100

    [NIJ] remove self-dependency as it was confusing, depend directly on sbt-utils

--- project/PluginBuild.scala
+++ project/PluginBuild.scala
@@ -13,27 +13,74 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import de.heikoseeberger.sbtheader.AutomateHeaderPlugin
+import de.heikoseeberger.sbtheader.HeaderKey._
 import sbt.Keys._
 import sbt._
 import uk.gov.hmrc.DefaultBuildSettings._
+import uk.gov.hmrc.SbtBuildInfo
+
+import scala.util.matching.Regex
 
 object PluginBuild extends Build {
 
   val pluginName = "sbt-auto-build"
-  val appVersion = "0.1.5-SNAPSHOT"
+  val pluginVersion = "0.1.5-SNAPSHOT"
+
+  private val standardSettings: Seq[Setting[_]] =
+    scalaSettings ++
+      SbtBuildInfo() ++
+      defaultSettings() ++
+      PublishSettings() ++
+      Resolvers() ++
+      ArtefactDescription()
+
 
   lazy val root = (project in file("."))
+    .enablePlugins(AutomateHeaderPlugin)
+    .settings(standardSettings)
     .settings(
-      sbtPlugin := true,
-      targetJvm := "jvm-1.7",
-      organization := "uk.gov.hmrc",
-      version := appVersion,
       name := pluginName,
+      version := pluginVersion,
+      sbtPlugin := true,
+      organization := "uk.gov.hmrc",
       scalaVersion := "2.10.4",
-      ArtefactDescription(),
-      addSbtPlugin("uk.gov.hmrc" % "sbt-utils" % "2.6.0"),
-      addSbtPlugin("de.heikoseeberger" % "sbt-header" % "1.4.1")
+      targetJvm := "jvm-1.7",
+      headers := HeaderSettings(),
+      addSbtPlugin("de.heikoseeberger" % "sbt-header" % "1.4.1"),
+      addSbtPlugin("uk.gov.hmrc" % "sbt-utils" % "2.6.0")
     )
+}
+
+object Resolvers {
+  def apply() =
+    resolvers := Seq(
+      Opts.resolver.sonatypeReleases,
+      Resolver.bintrayRepo("hmrc", "releases")
+    )
+}
+
+object PublishSettings {
+  def apply() = Seq(
+    publishArtifact := true,
+    publishArtifact in Test := false,
+    publishArtifact in IntegrationTest := false,
+    publishArtifact in(Test, packageDoc) := false,
+    publishArtifact in(Test, packageSrc) := false,
+    publishArtifact in(IntegrationTest, packageDoc) := false,
+    publishArtifact in(IntegrationTest, packageSrc) := false
+  )
+}
+
+object HeaderSettings {
+
+  import de.heikoseeberger.sbtheader.license.Apache2_0
+  import org.joda.time.DateTime
+
+  def apply(): Map[String, (Regex, String)] = {
+    Map("scala" -> Apache2_0(DateTime.now().getYear.toString, "HM Revenue & Customs"))
+  }
 }
 
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,6 @@
 resolvers += Resolver.url("hmrc-sbt-plugin-releases",
   url("https://dl.bintray.com/hmrc/sbt-plugin-releases"))(Resolver.ivyStylePatterns)
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "0.1.2")
+addSbtPlugin("uk.gov.hmrc" % "sbt-utils" % "2.6.0")
+
+addSbtPlugin("de.heikoseeberger" % "sbt-header" % "1.4.1")
