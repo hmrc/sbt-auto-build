2c6a8713f4962db3fdec81e43a76423a64540272
Duncan Crawford <duncan@tandemconsulting.eu>
2015-04-23 17:24:12 +0100

    add missing test

---
+++ src/test/scala/uk/gov/hmrc/ArtefactDescriptionSpec.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2015 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc
+
+import org.scalatest.{ShouldMatchers, WordSpec}
+
+class ArtefactDescriptionSpec extends WordSpec with ShouldMatchers {
+
+  "remote url" should {
+    "find the remote connection url" in {
+      ArtefactDescription.remoteConnectionUrl shouldBe "git@github.com:hmrc/sbt-auto-build.git"
+    }
+
+    "create the browser url" in {
+      ArtefactDescription.browserUrl shouldBe "https://github.com:hmrc/sbt-auto-build.git"
+    }
+  }
+
+}
