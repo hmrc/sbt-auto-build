8ddd537fd937560653c61bddadc7d3174267b22d
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2016-08-08 14:26:03 +0100

    [PS] Cascading updates from plugins down the chain which also depend on sbt-auto-build for their builds

--- project/PluginBuild.scala
+++ project/PluginBuild.scala
@@ -47,7 +47,7 @@ object PluginBuild extends Build {
       headers := HeaderSettings(),
       resolvers += Resolver.url("hmrc-sbt-plugin-releases", url("https://dl.bintray.com/hmrc/sbt-plugin-releases"))(Resolver.ivyStylePatterns),
       addSbtPlugin("de.heikoseeberger" % "sbt-header" % "1.5.0"),
-      addSbtPlugin("uk.gov.hmrc" % "sbt-settings" % "3.2.0"),
+      addSbtPlugin("uk.gov.hmrc" % "sbt-settings" % "3.3.0"),
       libraryDependencies ++= Seq(
         "org.eclipse.jgit" % "org.eclipse.jgit.pgm" % "3.7.0.201502260915-r",
         "org.scalatest" %% "scalatest" % "2.2.6" % "test",
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,8 +1,8 @@
 resolvers ++=
   Seq(Resolver.url("hmrc-sbt-plugin-releases", url("https://dl.bintray.com/hmrc/sbt-plugin-releases"))(Resolver.ivyStylePatterns))
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-settings" % "3.2.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-settings" % "3.3.0")
 
 addSbtPlugin("de.heikoseeberger" % "sbt-header" % "1.5.0")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "0.8.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "0.9.0")
